e51179e80d13ac03944fcef54c5dc93b63c22082
CaptainStrannik <lesergig@gmail.com>
2020-12-21 16:37:00 +0300

    234

--- main.bat
+++
@@ -1,7 +0,0 @@
-echo "# Strannik" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/CaptainStrannik/Strannik.git
-git push -u origin main
