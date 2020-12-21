1b9d15ffcb780cb2fb671a92e25edacd623f0780
Strannik <lesergig@gmail.com>
2020-12-21 16:14:33 +0300

    create project

---
+++ main.bat
@@ -0,0 +1,7 @@
+echo "# Strannik" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/CaptainStrannik/Strannik.git
+git push -u origin main
